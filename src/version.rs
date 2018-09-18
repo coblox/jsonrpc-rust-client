4d2e55cc65989c8de9879a81b6513feda1baf1d2
Lucas Soriano del Pino <l.soriano.del.pino@gmail.com>
2018-09-18 17:53:27 +1000

    Add test to check if response matches request
    Enable the use of JsonRPC v2 as a valid argument for the `RpcRequest`
    constructor, since `jsonrpc-http-server` and seemingly all other
    alternatives only handle version 2 requests.

--- src/version.rs
+++ src/version.rs
@@ -2,7 +2,6 @@
 pub enum JsonRpcVersion {
     #[serde(rename = "1.0")]
     V1,
-    // At the moment, only version 1 is supported.
-    //    #[serde(rename = "2.0")]
-    //    V2,
+    #[serde(rename = "2.0")]
+    V2,
 }
---
+++ tests/jsonrpc_calls.rs
@@ -0,0 +1,32 @@
+extern crate jsonrpc_client;
+extern crate jsonrpc_core;
+extern crate jsonrpc_http_server;
+extern crate spectral;
+
+use jsonrpc_client::*;
+use jsonrpc_core::*;
+use jsonrpc_http_server::*;
+use spectral::prelude::*;
+
+fn main() {}
+
+#[test]
+
+fn get_server_response() {
+    let mut io = IoHandler::new();
+    io.add_method("helloworld", |_: Params| {
+        Ok(Value::String("hello".to_string()))
+    });
+    let _server = ServerBuilder::new(io).start_http(&"127.0.0.1:3030".parse().unwrap());
+    let client = RpcClient::new(HTTPClient::new(), "http://127.0.0.1:3030");
+
+    let res =
+        client.send::<String, ()>(&RpcRequest::new0(JsonRpcVersion::V2, "test", "helloworld"));
+
+    assert_that(&res)
+        .is_ok()
+        .is_ok()
+        .is_equal_to(&String::from("hello"));
+
+    _server.unwrap().close();
+}
